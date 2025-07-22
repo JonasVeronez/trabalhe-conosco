0efc1516db010eb35b84e354d0a0010bfb3c46c6
Jonas Veronez <eng.jonasveronez@gmail.com.br>
2025-07-21 23:04:34 -0300

    fix(backend): update main.py with recent changes

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from routers import produtores, propriedade, cultura_safra, dashboard
 from database import create_db
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Cadastro de Produtores Rurais")
+
+# Configuração de CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:4200"],  # ou use ["*"] durante o desenvolvimento
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(produtores.router)
 app.include_router(propriedade.router)
